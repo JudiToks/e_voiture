c9631f3a08529a01b3d054bd958a53f5f2cf38e3
Judicaël Rakotoarisedy <judi.rakotoarisedy@gmail.com>
2024-01-27 01:04:50 +0300

    new 6

--- src/main/java/cloudy/e_voiture/repository/AnnonceRepository.java
+++ src/main/java/cloudy/e_voiture/repository/AnnonceRepository.java
@@ -3,8 +3,7 @@ package cloudy.e_voiture.repository;
 import cloudy.e_voiture.models.Annonce;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AnnonceRepository
-//        extends CrudRepository<Annonce, Integer>
+public interface AnnonceRepository extends CrudRepository<Annonce, Integer>
 {
-//    Annonce findById(int id_annonce);
+    Annonce findById(int id_annonce);
 }
