7193b24b74b966c74ac06718fee0c03b58b336cd
Judicaël Rakotoarisedy <judi.rakotoarisedy@gmail.com>
2024-01-27 01:59:47 +0300

    new 7

---
+++ src/main/java/cloudy/e_voiture/models/RegisterRequest.java
@@ -0,0 +1,21 @@
+package cloudy.e_voiture.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+    private String telephone;
+
+}
+
